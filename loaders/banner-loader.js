331766895287f3c6212bb9bb2a5bd48ffe1a5b30
max.liu <summarychm@hotmail.com>
2019-11-24 14:46:17 +0800

    📦 banner-loader加入了对sourceMap的支持

--- loaders/banner-loader.js
+++ loaders/banner-loader.js
@@ -1,9 +1,9 @@
 //@ts-check
 const webpack = require("webpack");
-const { getOptions } = require("loader-utils");
+const { getOptions, getCurrentRequest } = require("loader-utils");
 const validateOptions = require("schema-utils");
 const fs = require("fs");
-const sourceMap = require("source-map");
+const { SourceNode, SourceMapConsumer } = require("source-map");
 
 const schema = {
 	type: "object",
@@ -15,6 +15,7 @@ const schema = {
 	},
 	additionalProperties: false,
 };
+
 /**
  * 读取文件内容(Promise),路径为空则内容为"".
  * @param {string} filePath 文件绝对路径
@@ -27,9 +28,10 @@ function getFileContents(filePath) {
 		});
 	});
 }
+
 /**
  * @param {string | Buffer} [source]
- * @param {sourceMap.RawSourceMap} [map]
+ * @param {any} [map]
  * @param {any} [meta]
  */
 function banderLoader(source, map, meta) {
@@ -43,18 +45,33 @@ function banderLoader(source, map, meta) {
 	if (prefix || postfix) {
 		Promise.all([getFileContents(prefix), getFileContents(postfix)])
 			.then((data) => {
-				prefix && self.addDependency(prefix); // 将文件添加到依赖中
-				postfix && self.addDependency(postfix); // 将文件添加到依赖中
-				callback(null, data[0] + source + data[1]);
+				// 将文件添加到依赖中
+				prefix && self.addDependency(prefix);
+				postfix && self.addDependency(postfix);
+				finalCallBack(source, data[0], data[1]);
 			})
 			.catch(callback);
 	} else if (perStr || postStr) {
-		callback(null, perStr + source + postStr);
+		finalCallBack(perStr + source + postStr);
 	} else {
 		let noStrError = new Error("没有传递{prefix, postfix, perStr, postStr}中任意参数!");
 		callback(noStrError, source);
 	}
-	// TODO 支持用户传递sourceMpa的场景,更新sourceMap
-	// 学习如何在loader中生成sourceMap,并使用
+
+	// emit callback
+	async function finalCallBack(content, prefix, postfix) {
+		// 维护sourceMap
+		if (map) {
+			var currentRequest = getCurrentRequest(self);
+			var node = SourceNode.fromStringWithSourceMap(content, await new SourceMapConsumer(map));
+			node.prepend(prefix);
+			node.add(postfix);
+			var result = node.toStringWithSourceMap({
+				file: currentRequest,
+			});
+			map = result.map.toJSON();
+			callback(null, result.code, map);
+		} else callback(null, prefix + content + postStr);
+	}
 }
 module.exports = banderLoader;
