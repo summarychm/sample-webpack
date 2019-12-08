a6a2563b370343eb9181d7c29f74f19420b54243
max.liu <summarychm@hotmail.com>
2019-12-08 20:28:46 +0800

    📦 新建bundle-summary用于分析bundle文件逻辑.

---
+++ bundle-summary/0.bundle.js
@@ -0,0 +1,10 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([
+	[0],
+	{
+		"./src/moduleA.js": function(module, __webpack_exports__, __webpack_require__) {
+			"use strict";
+			__webpack_require__.r(__webpack_exports__);
+			/* harmony default export */ __webpack_exports__["default"] = "moduleA";
+		},
+	},
+]);
